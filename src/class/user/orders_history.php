d9739789338c76390a28a6cdffad793dd990398e
Lina <lina.meniai@edu.esiee-it.fr>
2024-02-02 00:13:36 +0100

    user order history fonctionnel

--- src/class/user/orders_history.php
+++ src/class/user/orders_history.php
@@ -1,29 +1,56 @@
 <?php 
 require_once '../../init.php';
 
-var_dump($_SESSION);
+$user_id = $_SESSION['user_id'];
 
-/*
 try {
     //////////////////////////
     // GET ALL USER ORDERS  //
     //////////////////////////
-    $pdoStatement = $bdd->prepare("SELECT * FROM articles;");
+    $pdoStatement = $bdd->prepare("SELECT * FROM orders WHERE customer_id = '$user_id';");
     $pdoStatement->execute();
-    $allProducts = $pdoStatement->fetchAll();
+    $allOrders = $pdoStatement->fetchAll();
     //var_dump($allProducts);
 } catch (PDOException $e) {
     $errMessage = $e->getMessage();
     echo $errMessage;
-} */
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <title> Order History </title>
-    <?php require_once SITE_ROOT.'src/partials/head_css.php' ; ?>
-</head>
+    <head>
+        <title> Order History </title>
+        <?php require_once SITE_ROOT.'src/partials/head_css.php' ; ?>
+    </head>
 
-<body>
+    <body>
+
+        <h1> Historique des commandes </h1>
+        <main id="table">
+            <table>
+                <thead>
+                    <tr>
+                        <th> N° de commande </th>
+                        <th> Date </th>
+                        <th> Prix total </th>
+                        <th> Adresse de facturation </th>
+                        <th> Statut </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($allOrders as $info) : ?>
+                        <tr>
+                            <td><?php echo $info[0]?></td>
+                            <td><?php echo $info[3]?></td>
+                            <td><?php echo $info[4]?></td>
+                            <td><?php echo $info[5]?></td>
+                            <td><?php echo $info[6]?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </main>
+    </body>
+</html>
