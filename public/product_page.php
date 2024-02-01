46d69721f0a7ee3e1d595abd469a1bb2dd2a67dc
MrRAnonybot <benildetob@gmail.com>
2024-02-01 10:31:24 +0100

    finishing the product page
    finaly

--- public/product_page.php
+++ public/product_page.php
@@ -20,49 +20,43 @@
       <img src="" alt="" class="absolute inset-0 w-full h-full object-cover rounded-lg" loading="lazy" />
     </div>
     <form class="flex-auto p-6">
+
       <?php
-      try {
-        //////////////////////////////
-        // GET ALL PRODUCTS FROM DB //
-        //////////////////////////////
-        $id = $_GET['id'];
-        $pdoStatement = $bdd->prepare("SELECT * FROM articles; WHERE id='$id'");
-        $pdoStatement->execute();
-        $allProducts = $pdoStatement->fetchAll();
-        //var_dump($allProducts);
-      } catch (PDOException $e) {
-        $errMessage = $e->getMessage();
-        echo $errMessage;
-      }
-      foreach ($allProducts as $infos) :
-        //var_dump($allProducts[0]);
+      $id = $_GET['id'];
+      //var_dump($_GET['id']);
+      $pdoStatement = $bdd->prepare("SELECT * FROM articles WHERE id='$id';");
+      $pdoStatement->execute();
+      $infos = $pdoStatement->fetch();
+      //var_dump($selectedProduct);
       ?>
 
 
-        <div class="flex flex-wrap items-baseline">
-          <h1 class="w-full flex-none mb-3 text-2xl leading-none text-slate-900">
-            <?php echo $infos['title'] ?>
-          </h1>
-          <div class="flex-auto text-lg font-medium text-slate-500">
-            <?php echo $infos['price'] ?>
+      <div class="flex flex-wrap items-baseline">
+        <h1 class="w-full flex-none mb-3 text-2xl leading-none text-slate-900">
+          <?php echo $infos['title'] ?>
+          <div class="card" style="width: 18rem;">
+            <img src="<?= '../' . $infos['photo_path'] ?>" class="card-img-top" alt="..." style=>
           </div>
-          <div class="text-xs leading-6 font-medium uppercase text-slate-500">
-            <?php echo $infos['stock'] ?>
-          </div>
+        </h1>
+        <div class="flex-auto text-lg font-medium text-slate-500">
+          <H3><?php echo $infos['price'] ?> €</H3>
         </div>
-        <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-          <div class="space-x-1 flex text-sm font-medium">
-          </div>
+        <div class="text-xs leading-6 font-medium uppercase text-slate-500">
+          <h4><?php echo $infos['stock'] ?> En Stock</h4>
         </div>
-        <div class="flex space-x-4 mb-5 text-sm font-medium">
-        <?php endforeach; ?>
+      </div>
+      <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+        <div class="space-x-1 flex text-sm font-medium">
+        </div>
+      </div>
+      <div class="flex space-x-4 mb-5 text-sm font-medium">
         <div class="flex-auto flex space-x-4 pr-4">
           <button class="flex-none w-1/2 h-12 uppercase font-medium tracking-wider border border-slate-200 text-slate-900" type="button">
             Add to cart
           </button>
         </div>
         </button>
-        </div>
+      </div>
     </form>
   </div>
 
@@ -86,8 +80,8 @@
       </div>
       <div class="card-body">
         <blockquote class="blockquote mb-0">
-          <p>A well-known quote, contained in a blockquote element.</p>
-          <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+          <p> <?= $infos['description'] ?></p>
+          <footer class="blockquote-footer">Someone famous in <cite title="Source Title">The Coding Factory</cite></footer>
         </blockquote>
       </div>
     </div>
